2d082e16b3a1ef9100aa888e1a234292a72c4406
sbiswas-s2 <sbiswas@singlestore.com>
2025-08-20 18:55:02 +0530

    Revert "fix(mobile): person birthday viewing/editing (#20731)"

--- mobile/lib/presentation/widgets/people/person_option_sheet.widget.dart
+++ mobile/lib/presentation/widgets/people/person_option_sheet.widget.dart
@@ -3,11 +3,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/translate_extensions.dart';
 
 class PersonOptionSheet extends ConsumerWidget {
-  const PersonOptionSheet({super.key, this.onEditName, this.onEditBirthday, this.birthdayExists = false});
+  const PersonOptionSheet({super.key, this.onEditName, this.onEditBirthday});
 
   final VoidCallback? onEditName;
   final VoidCallback? onEditBirthday;
-  final bool birthdayExists;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -26,7 +25,7 @@ class PersonOptionSheet extends ConsumerWidget {
             ),
             ListTile(
               leading: const Icon(Icons.cake),
-              title: Text((birthdayExists ? 'edit_birthday' : "add_birthday").t(context: context), style: textStyle),
+              title: Text('edit_birthday'.t(context: context), style: textStyle),
               onTap: onEditBirthday,
             ),
           ],
